c7de9469dd29c985fda33f27959ba772e5c113e2
Wilfred V <gesifred@gmail.com>
2023-07-31 18:11:30 -0400

    fix README and change to doc swagger

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('doc', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 4000;
